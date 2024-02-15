2332b48f20c61d165bf315ef6a69f055b9462259
vishal9128 <125853495+vishal9128@users.noreply.github.com>
2024-02-15 17:16:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+    // Get the button element by its ID
+    var butt = document.getElementById("butt");
+
+    // Add a click event listener to the button
+    butt.addEventListener("click", function() {
+        // Function to be executed when the button is clicked
+        alert("Thanks!");
+    });
+
+
+    var but = document.getElementById("but");
+    but.addEventListener("click", function() {
+        alert("Calling!");
+    });
+
+
+    var banch = document.getElementById("banch");
+    banch.addEventListener("click", function() {
+        alert("Calling!");
+    });
+    
+    
